a2fe1e9901b147c9e4b61b03bec7de4c68b486f5
Baptiste Lecocq <baptiste.lecocq@gmail.com>
2014-02-05 14:36:58 +0100

    Implement POST/PUT/DELETE method

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,11 +3,42 @@ class BookmarksController < ApplicationController
 
   def index
     @bookmarks = Bookmark.all
-    respond_with(@bookmarks)
+    respond_with @bookmarks
   end
 
   def show
-    @bookmark = Bookmark.find(params[:id])
-    respond_with(@bookmark)
+    @bookmark = Bookmark.find params[:id]
+    respond_with @bookmark
+  end
+
+  def create
+    @bookmark = Bookmark.new bookmark_params
+
+    if @bookmark.save
+      render json: { callback: { content: 'Bookmark created', success: true } }
+    else
+      render json: { callback: { content: @bookmark.errors.full_messages.to_sentence, success: false } }
+    end
+  end
+
+  def update
+    @bookmark = Bookmark.find params[:id]
+
+    if @bookmark.update bookmark_params
+      render json: { callback: { content: 'Bookmark updated', success: true } }
+    else
+      render json: { callback: { content: @bookmark.errors.full_messages.to_sentence, success: false } }
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id]).destroy
+    render json: { callback: { content: 'Bookmark deleted', success: true } }
+  end
+
+private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:title, :link)
   end
 end
